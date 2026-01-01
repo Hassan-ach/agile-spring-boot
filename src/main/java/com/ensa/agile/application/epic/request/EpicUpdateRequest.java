34d9c8d02735b002f07440b20768442e6e083cd6
bagi <hassanachlou@gmail.com>
2026-01-01 15:53:29 +0100

    add validation for epic update request

--- src/main/java/com/ensa/agile/application/epic/request/EpicUpdateRequest.java
+++ src/main/java/com/ensa/agile/application/epic/request/EpicUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.ensa.agile.application.epic.request;
 
+import com.ensa.agile.domain.global.exception.ValidationException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,14 +11,36 @@ import lombok.Data;
 public class EpicUpdateRequest {
     private String id;
     private String productId;
-    private final String title;
-    private final String description;
+    private String title;
+    private String description;
 
     public EpicUpdateRequest(String productId, String id,
                              EpicUpdateRequest req) {
+        if (req == null) {
+            throw new IllegalArgumentException("request cannot be null");
+        }
+
+        if (req.getTitle() == null && req.getDescription() == null) {
+            throw new ValidationException("title cannot be null or blank");
+        }
+
+        if (req.getTitle() != null) {
+            if (req.getTitle().isBlank()) {
+                throw new ValidationException("title cannot be blank");
+            } else {
+                this.title = req.getTitle();
+            }
+        }
+
+        if (req.getDescription() != null) {
+            if (req.getDescription().isBlank()) {
+                throw new ValidationException("description cannot be blank");
+            } else {
+                this.description = req.getDescription();
+            }
+        }
+
         this.productId = productId;
         this.id = id;
-        this.title = req.getTitle();
-        this.description = req.getDescription();
     }
 }
